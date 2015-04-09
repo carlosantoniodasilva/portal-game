8d07220eb95558f21722682aac9414a7318b575d
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2015-04-09 08:45:40 -0300

    Add simple test for push right/left

--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,7 +1,32 @@
 defmodule PortalTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "push right and left" do
+    Portal.shoot :orange
+    Portal.shoot :blue
+    portal = Portal.transfer :orange, :blue, [1, 2, 3]
+
+    assert Portal.Door.get(:orange) == [3, 2, 1]
+    assert Portal.Door.get(:blue) == []
+
+    Portal.push_left portal
+
+    assert Portal.Door.get(:orange) == [3, 2, 1]
+    assert Portal.Door.get(:blue) == []
+
+    Portal.push_right portal
+
+    assert Portal.Door.get(:orange) == [2, 1]
+    assert Portal.Door.get(:blue) == [3]
+
+    Portal.push_right portal
+
+    assert Portal.Door.get(:orange) == [1]
+    assert Portal.Door.get(:blue) == [2, 3]
+
+    Portal.push_left portal
+
+    assert Portal.Door.get(:orange) == [2, 1]
+    assert Portal.Door.get(:blue) == [3]
   end
 end
